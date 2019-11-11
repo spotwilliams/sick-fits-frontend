07b276266e3124834072b6d545d0ba4052c472df
Ricardo Vergara <ricardovergarawork@gmail.com>
2019-11-11 19:08:10 -0300

    Apollo with reactJs

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,33 @@
-import App, {Container} from 'next/app';
-import Page from '../components/Page';
+import App, { Container } from "next/app";
+import Page from "../components/Page";
+import { ApolloProvider } from "react-apollo";
+import withData from "../lib/withData";
 
 class MyApp extends App {
-    render() {
-        const {Component} = this.props;
-
-        return (
-            <Container>
-                <Page>
-                    <Component />
-                </Page>
-            </Container>
-        );
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {};
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
     }
+    // this exposes the query to the user
+    pageProps.query = ctx.query;
+    return { pageProps };
+  }
+  render() {
+    const { Component, apollo, pageProps } = this.props;
+    return (
+      <Container>
+        {/* ...Apollo will handle the state of the entire project */}
+        <ApolloProvider client={apollo}>
+          {/* Page is the highest level of our app */}
+          <Page>
+            {/* ...pageProps means that spread the object into their parts */}
+            <Component {...pageProps} />
+          </Page>
+        </ApolloProvider>
+      </Container>
+    );
+  }
 }
 
-export default MyApp;
+export default withData(MyApp);
