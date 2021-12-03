63a6db45acaf0625f52e2864f6594c43d5bda6cd
Richard Vergara <ricardo.vergara@adeva.co>
2021-12-03 12:44:42 -0300

    Progress minimal changes

--- components/Page.js
+++ components/Page.js
@@ -14,7 +14,7 @@ const GlobalStyles = createGlobalStyle`
     --black: #393939;
     --grey: #3A3A3A;
     --ligthGrey: #e1e1e1;
-    --offWhite: #ededed
+    --offWhite: #ededed;
     --maxWidth: 1000px;
     --bs: 0 12px 24px 0 rgba (0,0,0,0.09);
     box-sizing: border-box;
