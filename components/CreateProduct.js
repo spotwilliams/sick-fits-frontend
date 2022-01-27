c6346b8a7c4d3af60b76b30946b756810e614867
Richard Vergara <ricardo.vergara@adeva.co>
2022-01-27 09:11:55 -0300

    Creating product from mutation

--- components/CreateProduct.js
+++ components/CreateProduct.js
@@ -1,5 +1,32 @@
 import useForm from "../lib/useForm";
 import Form from "./styles/Form";
+import gql from "graphql-tag";
+import { useMutation } from "@apollo/client";
+import DisplayError from "./ErrorMessage";
+
+const CREATE_PRODUCT_MUTATION = gql`
+  mutation CREATE_PRODUCT_MUTATION(
+    $name: String!
+    $description: String!
+    $price: Int!
+    $image: Upload
+  ) {
+    createProduct(
+      data: {
+        name: $name
+        description: $description
+        status: "AVAILABLE"
+        price: $price
+        photo: { create: { image: $image, altText: $name } }
+      }
+    ) {
+      id
+      price
+      description
+      name
+    }
+  }
+`;
 
 export default function CreateProduct() {
   const { inputs, handleChange, resetForm, clearForm } = useForm({
@@ -9,14 +36,22 @@ export default function CreateProduct() {
     description: "cosito amazing",
   });
 
+  const [createProduct, { loading, error, data }] = useMutation(
+    CREATE_PRODUCT_MUTATION,
+    {
+      variables: inputs,
+    }
+  );
   return (
     <Form
-      onSubmit={(e) => {
+      onSubmit={async (e) => {
         e.preventDefault();
-        console.log(inputs);
+        await createProduct();
+        clearForm();
       }}
     >
-      <fieldset>
+      <DisplayError error={error} />
+      <fieldset disabled={loading} aria-busy={loading}>
         <label htmlFor="image">
           Image
           <input id="image" name="image" type="file" onChange={handleChange} />
